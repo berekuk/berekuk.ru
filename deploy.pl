b1ec88e2f7ddc5aa2da7516f8cfb09fdadfbf515
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2013-06-08 02:06:40 +0400

    magic push in deploy script

--- deploy.pl
+++ deploy.pl
@@ -6,4 +6,24 @@ use warnings;
 use IPC::System::Simple;
 use autodie qw(system);
 
-system(q{ssh ubuntu@berekuk.ru "sh -c 'cd berekuk.ru && git pull'"});
+# copy-pasted from questhub's deploy/ec2_deploy.pl
+sub check_local_repo {
+    my $git_branch = qx(git rev-parse --abbrev-ref HEAD);
+    chomp $git_branch;
+    unless ($git_branch eq 'master') {
+        die "Error: you should be on master branch to use --magic mode.\n";
+    }
+
+    my $git_status = qx(git status --short);
+    chomp $git_status;
+    if ($git_status) {
+        die "You've got some uncommited files:\n$git_status\n";
+    }
+}
+
+sub main {
+    system('git push origin master');
+    system(q{ssh ubuntu@berekuk.ru "sh -c 'cd berekuk.ru && git pull'"});
+}
+
+main unless caller;
