3121187f65b755eee61b3a1f051e38715b370f4f
Vyacheslav Matyukhin <me@berekuk.ru>
2018-10-01 00:37:05 +0300

    minor style tweaking

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,39 +1,54 @@
-import React from "react";
-import Link from "gatsby-link";
-import Helmet from "react-helmet";
+import React from 'react';
+import Link from 'gatsby-link';
+import Helmet from 'react-helmet';
 
 import Time from '../components/time';
+
+import styled from 'styled-components';
+
+const Preface = styled.div`
+  color: gray;
+`;
+
+const RssLink = () => (
+  <em>
+    <Link to="/rss.xml">RSS</Link>
+  </em>
+);
+
+const Article = ({ post }) => (
+  <article key={post.id}>
+    <h2>
+      <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+    </h2>
+    <Time>{post.frontmatter.date}</Time>
+    <p>{post.excerpt}</p>
+  </article>
+);
 
 export default function Index({ data }) {
   const { edges: posts } = data.allMarkdownRemark;
 
   return (
     <div>
-      <div style={{color: 'gray' }}>
+      <Preface>
         <p>
-          Новая версия блога (которую вы видите ниже) на движке Gatsby.js пока в разработке. Старая версия блога — на <a href="https://blog.berekuk.ru">blog.berekuk.ru</a>.
+          Новая версия блога (которую вы видите ниже) на движке Gatsby.js пока в
+          разработке. Старая версия блога — на{' '}
+          <a href="https://blog.berekuk.ru">blog.berekuk.ru</a>.
         </p>
         <p>
-          TODO: перенести комментарии, <s>перенести теги</s>, настроить редиректы, перенести картинки, <s>допилить стили</s>, <s>сделать RSS</s>.
+          TODO: перенести комментарии, настроить редиректы, перенести картинки.
         </p>
         <p>
-          <em><Link to="/rss.xml">RSS</Link></em>
+          <RssLink />
         </p>
-      </div>
-      <div className="blog-posts">
+        <hr />
+      </Preface>
+      <div>
         {posts
           .filter(post => post.node.frontmatter.title.length > 0)
-          .map(({ node: post }) => {
-            return (
-              <article className="blog-post-preview" key={post.id}>
-                <h2>
-                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
-                </h2>
-                <Time>{post.frontmatter.date}</Time>
-                <p>{post.excerpt}</p>
-              </article>
-            );
-          })}
+          .map(({ node }) => <Article post={node} />)}
       </div>
     </div>
   );
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import Helmet from "react-helmet";
+import Helmet from 'react-helmet';
 
 import Disqus from 'disqus-react';
 import styled from 'styled-components';
@@ -17,6 +17,10 @@ const FooterLinks = styled.div`
   justify-content: space-between;
 `;
 
+const PostHeader = styled.header`
+  margin-bottom: 30px;
+`;
+
 export default function Template({ data, pathContext }) {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
@@ -27,18 +31,24 @@ export default function Template({ data, pathContext }) {
   const disqusConfig = {
     url: `${metadata.siteUrl}/${frontmatter.path}`,
     identifier: frontmatter.path,
-  }
+  };
 
   return (
     <div>
       <Helmet title={`${frontmatter.title} | ${metadata.title}`} />
-      <h1>{frontmatter.title}</h1>
-      <Time>{frontmatter.date}</Time>
+
+      <PostHeader>
+        <h1>{frontmatter.title}</h1>
+        <Time>{frontmatter.date}</Time>
+      </PostHeader>
+
       <div dangerouslySetInnerHTML={{ __html: html }} />
 
       <hr />
+
       <PostFooter>
-        {frontmatter.categories && frontmatter.categories.map(e => `#${e}`).join(', ')}
+        {frontmatter.categories &&
+          frontmatter.categories.map(e => `#${e}`).join(', ')}
         <FooterLinks>
           {prev && <Link to={prev.path}>&larr; {prev.title}</Link>}
           {next && <Link to={next.path}>{next.title} &rarr;</Link>}
